5e3acc0adb1dcb670702b0ad15768b9d6ac50e6e
Dhruv Patel <169485603+dhruvp987@users.noreply.github.com>
2025-10-18 13:30:44 -0400

    Create test function and endpoint

--- cdk/cdk/cdk_stack.py
+++ cdk/cdk/cdk_stack.py
@@ -1,7 +1,7 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_apigateway as apigw,
+    aws_lambda as _lambda
 )
 from constructs import Construct
 
@@ -10,10 +10,17 @@ class CdkStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        fn = _lambda.Function(
+            self,
+            "TestFunction",
+            runtime=_lambda.Runtime.PYTHON_3_13,
+            handler="test_function.lambda_handler",
+            code=_lambda.Code.from_asset("lambda")
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        endpoint = apigw.LambdaRestApi(
+            self,
+            "ApiGwTest",
+            handler=fn,
+            rest_api_name="test"
+        )
