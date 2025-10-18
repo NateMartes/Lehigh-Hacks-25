586f9baaa01caa949e201ea8992811eaf269348a
Zack <ichmagkase70@gmail.com>
2025-10-18 16:48:21 -0400

    added bedrock test lambda

--- cdk/cdk/cdk_stack.py
+++ cdk/cdk/cdk_stack.py
@@ -2,23 +2,25 @@ from aws_cdk import (
     Stack,
     aws_apigateway as apigw,
     aws_dynamodb as dynamodb,
-    aws_lambda as _lambda
+    aws_lambda as _lambda,
 )
 from constructs import Construct
 
-class CdkStack(Stack):
 
+class CdkStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        chapters_table = dynamodb.Table.from_table_name(self, "ChaptersTable", "Chapters")
+        chapters_table = dynamodb.Table.from_table_name(
+            self, "ChaptersTable", "Chapters"
+        )
 
         test_fn = _lambda.Function(
             self,
             "TestFunction",
             runtime=_lambda.Runtime.PYTHON_3_13,
             handler="test_function.lambda_handler",
-            code=_lambda.Code.from_asset("lambda")
+            code=_lambda.Code.from_asset("lambda"),
         )
 
         new_chapter_fn = _lambda.Function(
@@ -26,7 +28,7 @@ class CdkStack(Stack):
             "NewChapterFunction",
             runtime=_lambda.Runtime.PYTHON_3_13,
             handler="new_chapter_function.lambda_handler",
-            code=_lambda.Code.from_asset("lambda")
+            code=_lambda.Code.from_asset("lambda"),
         )
         chapters_table.grant_read_write_data(new_chapter_fn)
 
@@ -35,30 +37,31 @@ class CdkStack(Stack):
             "TestDynDBFunction",
             runtime=_lambda.Runtime.PYTHON_3_13,
             handler="test_dyndb_function.lambda_handler",
-            code=_lambda.Code.from_asset("lambda")
+            code=_lambda.Code.from_asset("lambda"),
         )
         chapters_table.grant_read_write_data(test_dyndb_fn)
 
-        api = apigw.RestApi(
+        test_bedrock_fn = _lambda.Function(
             self,
-            "LehighApiGw",
-            rest_api_name="LehighApiGw"
+            "TestBedrockFunction",
+            runtime=_lambda.Runtime.PYTHON_3_13,
+            handler="test_bedrock_function.lambda_handler",
+            code=_lambda.Code.from_asset("lambda"),
         )
+        chapters_table.grant_read_write_data(test_dyndb_fn)
+
+        api = apigw.RestApi(self, "LehighApiGw", rest_api_name="LehighApiGw")
 
         test_resource = api.root.add_resource("test")
-        test_resource.add_method(
-            "GET",
-            apigw.LambdaIntegration(test_fn)
-        )
+        test_resource.add_method("GET", apigw.LambdaIntegration(test_fn))
 
         new_resource = api.root.add_resource("new")
-        new_resource.add_method(
-            "POST",
-            apigw.LambdaIntegration(new_chapter_fn)
-        )
+        new_resource.add_method("POST", apigw.LambdaIntegration(new_chapter_fn))
 
         test_dyndb_resource = api.root.add_resource("testdyndb")
-        test_dyndb_resource.add_method(
-            "POST",
-            apigw.LambdaIntegration(test_dyndb_fn)
+        test_dyndb_resource.add_method("POST", apigw.LambdaIntegration(test_dyndb_fn))
+
+        test_bedrock_resource = api.root.add_resource("testbdrk")
+        test_bedrock_resource.add_method(
+            "GET", apigw.LambdaIntegration(test_bedrock_fn)
         )
