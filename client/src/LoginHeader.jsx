8b68f97fbbcb07c3c1b7d6ee4c2493726ebfb820
Nathaniel Martes <nathaniel.martes@wilkes.edu>
2025-10-18 21:32:05 -0400

    feat: Add header

---
+++ client/src/LoginHeader.jsx
@@ -0,0 +1,9 @@
+function LoginHeader() {
+    return (
+        <div className="flex flex-col place-items-center text-2xl lg:text-3xl font-bold p-6">
+            <h1>Lehigh Hacks</h1>
+        </div>
+    );
+}
+
+export default LoginHeader;
